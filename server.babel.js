35d02b6f5e0daaa648cc3137af116f6ea660481e
JT <jbowler2400@gmail.com>
2016-08-04 17:16:06 -0700

    Plug in pug

--- server.babel.js
+++ server.babel.js
@@ -3,5 +3,11 @@ import express from 'express';
 const app = express();
 
 app.use('/', express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
 
 app.listen(process.env.PORT || 8000);
